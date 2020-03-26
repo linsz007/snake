98fce8f2bcb11f8fbfcfd2a556fff71d3a6a5c21
linshizhe123 <1342916880@qq.com>
2020-03-26 11:03:05 +0800

    转向逻辑

--- snake/src/app/snake/snake.component.ts
+++ snake/src/app/snake/snake.component.ts
@@ -10,7 +10,7 @@ export class SnakeComponent implements OnInit, OnDestroy {
 
   @ViewChild('canvas', {static: true}) private c: ElementRef;
 
-  public snakeList: Array<snakeList> = [];
+  public snakeList: Array<SnakeItem> = [];
 
   private timeSubscription: Subscription; //定时器
   private keyboardSubscription: Subscription; // 键盘监听
@@ -23,7 +23,7 @@ export class SnakeComponent implements OnInit, OnDestroy {
 
   public startOrReset = '开始';
 
-  public direction: Array<number> = [0, 24]; // 控制方向 ：x和y分别加上这个数组的元素
+  public currentDirectionArr: Array<number> = [0, 24]; // 当前控制方向 ：x和y分别加上这个数组的元素
 
   public canvas: any;
 
@@ -34,8 +34,9 @@ export class SnakeComponent implements OnInit, OnDestroy {
     this.listenKeyboard(); // 注册键盘监听
 
     this.snakeList = [];
-    this.snakeList.push({x: 4, y: 4, w: 16, h: 16});
-    this.snakeList.push({x: 4, y: 28, w: 16, h: 16});
+    this.snakeList.push({x: 4, y: 4, w: 16, h: 16, directionArr: [0, 24]});
+    this.snakeList.push({x: 4, y: 28, w: 16, h: 16, directionArr: [0, 24]});
+    this.currentDirectionArr = [0, 24];
 
 
     this.canvas = this.c.nativeElement.getContext('2d');
@@ -64,9 +65,9 @@ export class SnakeComponent implements OnInit, OnDestroy {
       this.time = this.suspendTime;
     }
     // 定时器启动
-    this.timeSubscription = timer((startOrReset !== '继续' ? 0 : 1000), 1000).subscribe(sec => {
+    this.timeSubscription = timer(1000, 1000).subscribe(sec => {
       this.refreshCanvas();
-      this.time = startOrReset !== '继续' ? sec : sec + this.suspendTime + 1;
+      this.time = startOrReset !== '继续' ? sec + 1 : sec + this.suspendTime + 1;
     });
   }
 
@@ -93,14 +94,6 @@ export class SnakeComponent implements OnInit, OnDestroy {
   }
 
   /**
-   * 清除矩形
-   * @param x,y
-   */
-  public clearRect(x: number, y: number) {
-    this.canvas.clearRect(x, y, 16, 16);
-  }
-
-  /**
    * 清楚画布
    */
   public clearCanvas() {
@@ -108,21 +101,26 @@ export class SnakeComponent implements OnInit, OnDestroy {
   }
 
   /**
-   * 刷新画布 每秒一次 更新位置
-   * @param isContinue:boolean 是否为继续
+   * 刷新画布,每秒一次,更新位置
+   * 原理：遍历元素，最后一个元素（头部）方向由this.currentDirectionArr控制，
+   * 其他的元素（身体）由前一个元素的directionArr控制
    */
   public refreshCanvas() {
     this.clearCanvas();
     for (let i = 0; i < this.snakeList.length; i++) {
       if (i === this.snakeList.length - 1) { // 判断是否是头部
-        this.drawNewRect(this.snakeList[i].x + this.direction[0],
-          this.snakeList[i].y + this.direction[0], true);
+        this.drawNewRect(this.snakeList[i].x + this.currentDirectionArr[0],
+          this.snakeList[i].y + this.currentDirectionArr[1], true);
+        this.snakeList[i].directionArr = this.currentDirectionArr;
+        this.snakeList[i].x = this.snakeList[i].x + this.currentDirectionArr[0];
+        this.snakeList[i].y = this.snakeList[i].y + this.currentDirectionArr[1];
       } else {
-        this.drawNewRect(this.snakeList[i].x + this.direction[0],
-          this.snakeList[i].y + this.direction[0], false);
+        this.drawNewRect(this.snakeList[i].x + this.snakeList[i + 1].directionArr[0],
+          this.snakeList[i].y + this.snakeList[i + 1].directionArr[1], false);
+        this.snakeList[i].directionArr = this.snakeList[i + 1].directionArr;
+        this.snakeList[i].x = this.snakeList[i].x + this.snakeList[i + 1].directionArr[0];
+        this.snakeList[i].y = this.snakeList[i].y + this.snakeList[i + 1].directionArr[1];
       }
-      this.snakeList[i].x = this.snakeList[i].x + this.direction[0];
-      this.snakeList[i].y = this.snakeList[i].y + this.direction[1];
     }
   }
 
@@ -130,22 +128,24 @@ export class SnakeComponent implements OnInit, OnDestroy {
    * 键盘事件
    */
   public onKeyDown(key: string) {
-    switch (key) {
-      case 'ArrowUp':
-
-        break;
-      case 'ArrowRight':
-
-        break;
-      case 'ArrowDown':
-        this.onKeyDown(event.key);
-        break;
-      case 'ArrowLeft':
-        
-        break;
-      case ' ':
-
-        break;
+    // 转向逻辑
+    if (this.currentDirectionArr.toString() === [-24, 0].toString() ||
+      this.currentDirectionArr.toString() === [24, 0].toString()) {
+      if (key === 'ArrowUp') {
+        this.currentDirectionArr = [0, -24];
+      } else if (key === 'ArrowDown') {
+        this.currentDirectionArr = [0, 24];
+      } else {
+        return;
+      }
+    } else {
+      if (key === 'ArrowRight') {
+        this.currentDirectionArr = [24, 0];
+      } else if (key === 'ArrowLeft') {
+        this.currentDirectionArr = [-24, 0];
+      } else {
+        return;
+      }
     }
   }
 
@@ -153,6 +153,7 @@ export class SnakeComponent implements OnInit, OnDestroy {
    * 键盘监听
    */
   private listenKeyboard() {
+    this.keyboardSubscription && this.keyboardSubscription.unsubscribe();
     this.keyboardSubscription = fromEvent(window, 'keydown').subscribe((event: any) => {
       this.onKeyDown(event.key);
     });
@@ -160,9 +161,10 @@ export class SnakeComponent implements OnInit, OnDestroy {
 
 }
 
-class snakeList {
+class SnakeItem {
   x: number;
   y: number;
   w: number;
   h: number;
+  directionArr: Array<number> = [0, 24];
 }
